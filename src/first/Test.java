c886378bcddc60835584015ce22ffbb588247aca
YuriiS-68 <yurii_s@bigmir.net>
2017-07-07 20:33:12 +0300

    test commit

---
+++ src/first/Test.java
@@ -0,0 +1,11 @@
+package first;
+
+/**
+ * Created by Skorodielov on 07.07.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("I am created first step");
+        System.out.println("......");
+    }
+}
---
+++ src/third/Test.java
@@ -0,0 +1,11 @@
+package third;
+
+/**
+ * Created by Skorodielov on 07.07.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Finish");
+        System.out.println("......");
+    }
+}
